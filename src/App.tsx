c5d21f34ecceb1b2d3f90a4c5230f3c0c537db12
tbsoysal <berksysl.g@gmail.com>
2025-02-20 23:26:47 +0300

    Get input from user

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 type Todo = {
   id: number;
   text: string;
   isDone: boolean;
-}
+};
 
 function App() {
   const [newTodo, setNewTodo] = useState("");
@@ -12,35 +12,46 @@ function App() {
 
   const addTodo = () => {
     if (newTodo.trim() === "") return;
-    
+
     const newTask: Todo = {
       id: Date.now(),
       text: newTodo,
-      isDone: false
-    }
-    
+      isDone: false,
+    };
+
     setTodos([...todos, newTask]);
     setNewTodo("");
-  }
+  };
+
+  const listTodos = (): ReactNode => {
+    if (todos.length) {
+      todos.map((todo) => <li>{todo.text}</li>);
+    }
+  };
 
   return (
     <div className="min-h-screen text-center sf-pro-font py-8 bg-[#F5F5F7]">
       <h1 className="font-bold text-[28px] text-[#000000] mt-4">Today</h1>
-      <p className="font-medium text-[14px] text-[#8E8E93] mb-12">Your tasks for today</p>
+      <p className="font-medium text-[14px] text-[#8E8E93] mb-12">
+        Your tasks for today
+      </p>
       <div className="mx-12">
         <input
           className="block w-full shadow-[0_4px_10px_rgba(0,0,0,0.15)] outline-none rounded-[16px] bg-[white] text-[16px] px-[16px] h-[54px]"
           placeholder="Add a new task"
           type="text"
           value={newTodo}
-          onChange={(e)=>setNewTodo(e.target.value)}
+          onChange={(e) => setNewTodo(e.target.value)}
         />
-        <button onClick={addTodo} className="bg-[#007AFF] w-[40px] h-[40px] mt-12 rounded-full font-bold text-white text-[20px]">+</button>
+        <button
+          onClick={addTodo}
+          className="bg-[#007AFF] w-[40px] h-[40px] mt-12 rounded-full font-bold text-white text-[20px]"
+        >
+          +
+        </button>
       </div>
       <ul>
-        {todos.map(todo => {
-
-        })}
+        { listTodos() }
       </ul>
     </div>
   );
