53a3f696366e7360357f793dfa6d0b59e431d4e3
tbsoysal <berksysl.g@gmail.com>
2025-02-21 21:47:00 +0300

    Add todo items and toggle

--- src/App.tsx
+++ src/App.tsx
@@ -23,11 +23,30 @@ function App() {
     setNewTodo("");
   };
 
-  const listTodos = (): ReactNode => {
-    if (todos.length) {
-      todos.map((todo) => <li>{todo.text}</li>);
-    }
+   const toggleTodo = (id: number) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
   };
+
+  function listTodos() {
+    if (todos.length > 0) {
+      return todos.map((todo) => {
+        return (
+          <li key={todo.id} className="bg-white flex align-center w-[90%] mx-auto rounded-3xl my-4 p-6">
+            <div onClick={() => toggleTodo(todo.id)} className={`w-[30px] h-[30px] rounded-full border-1 border-[#D1D1D6] mr-3 ${todo.isDone ? "bg-black": "bg-none"}`}></div>
+            <p className={`text-[22px] ${todo.isDone ? "line-through": ""}`}>{todo.text}</p>
+            <div className="ml-auto flex">
+              <button><img className="w-[16px]" src="/trash.svg" alt="trash icon" /></button>
+              <button><img className="w-[16px] ml-4" src="/pen.svg" alt="" /></button>
+            </div>
+          </li>
+        );
+      });
+    }
+  }
 
   return (
     <div className="min-h-screen text-center sf-pro-font py-8 bg-[#F5F5F7]">
@@ -35,7 +54,7 @@ function App() {
       <p className="font-medium text-[14px] text-[#8E8E93] mb-12">
         Your tasks for today
       </p>
-      <div className="mx-12">
+      <div className="mx-12 mb-12">
         <input
           className="block w-full shadow-[0_4px_10px_rgba(0,0,0,0.15)] outline-none rounded-[16px] bg-[white] text-[16px] px-[16px] h-[54px]"
           placeholder="Add a new task"
@@ -50,9 +69,7 @@ function App() {
           +
         </button>
       </div>
-      <ul>
-        { listTodos() }
-      </ul>
+      <ul>{listTodos()}</ul>
     </div>
   );
 }
