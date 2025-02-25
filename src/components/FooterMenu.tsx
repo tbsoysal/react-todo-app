a6c0be71f8e4b21cb1c6ec35efa00df478c63264
tbsoysal <berksysl.g@gmail.com>
2025-02-25 22:54:06 +0300

    update menu

--- src/components/FooterMenu.tsx
+++ src/components/FooterMenu.tsx
@@ -7,7 +7,7 @@ function FooterMenu({ menuIndex, setMenuIndex }: Props) {
   const activeMenuStyle = `text-black relative after:content-[""] after:absolute after:w-12 after:h-2 after:bg-[#007AFF] after:rounded-2xl after:bottom-2.5 after:left-[50%] after:translate-x-[-50%]`;
 
   return (
-    <footer className="absolute bottom-0 w-screen bg-white flex justify-center px-4 max-w-lg left-[50%] translate-x-[-50%]">
+    <footer className="fixed bottom-0 w-screen bg-white flex justify-center px-4 max-w-lg left-[50%] translate-x-[-50%]">
       <a
         onClick={() => setMenuIndex(0)}
         className={`py-5 flex-1 text-[#8E8E93] ${menuIndex === 0 && activeMenuStyle}`}
